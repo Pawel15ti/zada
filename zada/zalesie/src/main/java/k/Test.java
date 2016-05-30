860bb018da31b8c14f9754c8efd09626f2ca7f90
Pawcio <Pawcio@Pawcio-Komputer>
2016-05-30 11:53:17 +0200

    jvhjvhj 11111

---
+++ zada/zalesie/src/main/java/k/Test.java
@@ -0,0 +1,15 @@
+package k;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Licz licz=new Licz();
+		System.out.println(licz);
+		
+		Licz l = new Licz();
+		System.out.println(l.liczymy(3, 4));
+
+	}
+
+}

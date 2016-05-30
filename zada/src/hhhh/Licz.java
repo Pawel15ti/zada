6b68b7c133305f36a26f99cdc61b6c54c3597daf
Pawcio <Pawcio@Pawcio-Komputer>
2016-05-30 11:50:13 +0200

    ghgvhg

---
+++ zada/src/hhhh/Licz.java
@@ -0,0 +1,21 @@
+package hhhh;
+
+public class Licz {
+	private int a=0;
+	private int b=0;
+	public Licz() {
+		this.a=1;
+	}
+
+	public  double liczymy(int a,int b){
+		System.out.println("Licze sume:");
+	return a+b;	
+	}
+
+	@Override
+	public String toString() {
+		return "Licz [a=" + a + ", b=" + b + "]";
+	}
+	
+	
+}
---
+++ zada/src/hhhh/Test.java
@@ -0,0 +1,15 @@
+package hhhh;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Licz licz=new Licz();
+		System.out.println(licz);
+		
+		Licz l = new Licz();
+		System.out.println(l.liczymy(9, 4));
+
+	}
+
+}
